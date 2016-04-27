6e37db6ee31d5e2eef3ed47319c7f538e8b42cb7
Arlen Neylon <aneylon@users.noreply.github.com>
2016-04-26 17:24:16 -0700

    Added files via upload

--- tmp/app.js
+++ tmp/app.js
@@ -1,103 +1,84 @@
 $(document).ready(function(){
-  var updateSpeed = 2000;
-  var currentStream = "";
-  var currentTwittles = 0;
+  var updateSpeed = 500;
+  var defaultStream = streams.home;
+  var currentStream = defaultStream;
+  var currentTweets = 0;
   visitor = "visitor";
   streams.users.visitor = [];
 
-  setNumberOfFollowers();
-  setNumberFollowing();
-  update();
+  update(currentStream);
 
-  setInterval(function(){checkForNew()},updateSpeed);
+  setInterval(function(){checkForNew(currentStream)},updateSpeed);
+
+  $("body").on("click",".userName",function(){
+    var selectedUser = $(this).text();
+    currentStream = streams.users[selectedUser];
+    update(currentStream);
+  });
   
-  $("#statusUpdate").on("keydown",function(event){
+  $("#newInputText").on("keydown",function(event){
     if(event.which === 13) { // check if enter key is pressed.
-      writeTweet($(this).val());
-      $(this).val("");
-      update();
+      addNewTweet();
     }
   });
 
-  $("#statusUpdateButton").on("click",function(){
-    writeTweet($("#statusUpdate").val());
-    $("#statusUpdate").val("");
-    update();
+  $("#newInputButton").on("click",function(){
+    addNewTweet();
   });
 
-  $("#loadNew").on("click",function(){
-    update();
-    $(this).slideUp("fast");
+  $("#showNew").on("click",function(){
+    update(currentStream);
   });
 
-  $("#goHome").on("click",function(){
-    goHome();
+  $("#showAll").on("click",function(){
+    currentStream = defaultStream;
+    update(currentStream);
   });
 
-  $("#loginButton").on("click",function(){
-    $("#signin").slideToggle("slow");
-    $("#header").slideToggle("slow");
-    $("#sendTwittle").slideToggle("slow");
-  });
+  function addNewTweet(){
+    var updateText = $("#newInputText").val();
+    if(updateText !== "") {
+      writeTweet(updateText);
+      $("#newInputText").val("");
+      update(currentStream);
+    }
+  }
 
   function checkForNew(stream){
-    var newNumber = streams.home.length;
-    var diff = newNumber - currentTwittles;
-    if(currentTwittles < newNumber){
-      $("#loadNew").slideDown("fast");
-      $("#loadNew").text("load " + diff + " new twittles");
+    var newNumber = stream.length;
+    var diff = newNumber - currentTweets;
+    if(currentTweets < newNumber){
+      $("#showNewText").text("Load " + diff + " new tweets");
+    } else {
+      $("#showNewText").text("Load new tweets");
     }
   }
 
   function update(stream){
-    currentTwittles = streams.home.length;
+    currentTweets = stream.length;
+    var $tweets = $("#tweets");
+    $tweets.html('');
 
-    var $twittles = $("#twittles");
-    $twittles.html('');
-
-    var index = streams.home.length - 1;
+    var index = stream.length - 1;
     while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $("<div class='twittle all'></div>");
-      var $userLink = $("<p class='userLink'></p>");
-      var $user = $("<span class='userName'></span> ");
-      $user.text("@" + tweet.user + " ");
-      var $time = $("<span class='timeStamp'></span>");
-      var time = moment(tweet.created_at).fromNow();
+      var tweet = stream[index];
+      var $tweet = $("<div class='tweet padTwentyFive overflowAuto'></div>");
+      var $userWrapper = $("<div class='widthTwenty centerText left'></div>");
+      var $icon = $("<div id='icon' class='darkGreyBack'></div>");
+      var $user = $("<p class='userName pointer blueText boldFont'></p> ");
+      $user.text(tweet.user);
+      var $time = $("<p class='blueText'></p>");
+      var time = " " + moment(tweet.created_at).fromNow();
       $time.text(time);
-      var $message = $("<p class='message'></p>");
+      var $message = $("<div class='right message bigFont whiteBack padFive widthEighty heightOneHundred'></div>");
       $message.text(tweet.message);
-      $user.appendTo($userLink);
-      $time.appendTo($userLink);
-      $userLink.appendTo($tweet);
+      $icon.appendTo($userWrapper);
+      $user.appendTo($userWrapper);
+      $time.appendTo($userWrapper);
+      $userWrapper.appendTo($tweet);
       $message.appendTo($tweet);
-      $tweet.appendTo($twittles);
+      $tweet.appendTo($tweets);
       index -= 1;
     }
   }
-
-  function setNumberOfFollowers(){
-    var $followers = $("#numFollowers");
-    $followers.text(Math.floor(Math.random() * 200));
-  }
-
-  function setNumberFollowing(){
-    var $following = $("#numFollowing");
-    var numUsers = 0;
-    for(var user in streams.users){
-      numUsers ++;
-    }
-    numUsers -= 1; // remove one for current user.
-    $following.text(numUsers);
-  }
-
-  function setNumberOfTwittles(){
-    $("#numTwittles").text(streams.users[curUser]);
-  }
-
-  function goHome(){
-    console.log("go home");
-    // set stream back to all and call update?
-  }
-
 });
