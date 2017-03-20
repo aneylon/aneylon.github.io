53791402a5e391ebe6ee10435776f08b597cb16f
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-03-19 20:53:14 -0600

    add and update thumbnails

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 function load(){
-  loadElement(projects, projTmp, 'projectsContent')
-  loadElement(images, imgTmp, 'imagesContent')
+  // loadElement(test, testTmp, 'testContent')
+  // loadElement(projects, projTmp, 'projectsContent')
+  // loadElement(images, imgTmp, 'imagesContent')
 }
 
 function loadElement(data, template, elementId){
   console.log(data)
+  var output = ''
+  for(var key in data){
+
+  }
+  document.getElementById(elementId).innerHTML = 'this is a test'
 }
+
+var test = [
+  {content: 'stuff'},
+  {content: 'moreStuff'},
+  {content: 'evenMoreStuff'}
+]
+
+var testTmp = `<li></li>`
 
 var projects = [
   {name: 'one-name', desc: 'one-desc', imgUrl: 'one-img', link: 'one-link', github: 'one-git'},
